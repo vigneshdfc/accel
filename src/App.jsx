67d05ac8a7afd8ac91222c508002c22b0d7e2819
28112001 <aaronstains2001@gmail.com>
2022-11-19 18:02:55 +0530

    feat: Header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Home from "./pages/relationship/home";
+
+function App() {
+  return (
+    <>
+      <React.Fragment>
+        <Home />
+      </React.Fragment>
+    </>
+  );
+}
+export default App;
---
+++ src/pages/relationship/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Relationship from "./index";
+
+function Home() {
+  return (
+    <>
+      <Relationship />
+    </>
+  );
+}
+export default Home;
