6021df6cc7726218f4167d48b00a2a498a311324
vigneshdfc <vigneshdfc02@gmail.com>
2022-11-21 18:00:34 +0530

    feat

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import SimpleSlider from "../src/components/carausel/carausel";
 import Sidebar from "./components/sidenav/sidenav";
+import Head from "./components/header/header";
+import Hero from "./components/hero/hero";
 
 const App = () => {
   return (
-    <div>
+    <React.Fragment>
       <SimpleSlider />
       <Sidebar />
-    </div>
+      <Head />
+      <Hero />
+    </React.Fragment>
   );
 };
 export default App;
---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./hero.css";
+import add from "../../assets/video/addmp4.mp4";
+import clothadd from "../../assets/video/clothadd.mp4";
+import search from "../../assets/Images/icons/searchicon.png";
+
+const Hero = () => {
+  return (
+    <>
+      <div className="hero">
+        <div className="hero_slide">
+          <h1>First partner to exceptional teams everywhere</h1>
+          <div className="search-bar">
+            <div className="search">
+              <input
+                type="text"
+                value="SEARCH FOR A COMPANY,FOUNDER,OR PARTNER"
+              />
+            </div>
+            <div className="searchicon">
+              <img src={search} alt="search" />
+            </div>
+          </div>
+        </div>
+
+        <div className="brand_adv">
+          <div className="video">
+            <video controls autoplay muted id="myvideo">
+              <source src={add} type="video/mp4" />
+            </video>
+          </div>
+          <div className="myvideo">
+            <video controls autoplay muted id="myvideo">
+              <source src={clothadd} type="video/mp4" />
+            </video>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Hero;
